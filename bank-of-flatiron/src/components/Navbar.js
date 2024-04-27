a42c50501134ce76e096ab869b7d2faba3ae440b
mendarrr <sarahabby541@gmail.com>
2024-04-27 15:32:39 +0300

    Adding Components and updating them

---
+++ bank-of-flatiron/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div id="navbar" className='nav'>
+        <h1 class="title">  The Royal Bank of Flatiron</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ bank-of-flatiron/src/components/Transaction.js
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import "./Transaction.css";
+
+const Transaction = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [newTransaction, setNewTransaction] = useState({
+    date: "",
+    description: "",
+    category: "",
+    amount: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewTransaction({ ...newTransaction, [name]: value });
+  };
+
+  const addTransaction = (e) => {
+    e.preventDefault();
+    if (
+      newTransaction.date &&
+      newTransaction.description &&
+      newTransaction.category &&
+      newTransaction.amount
+    ) {
+      setTransactions([...transactions, newTransaction]);
+      setNewTransaction({
+        date: "",
+        description: "",
+        category: "",
+        amount: "",
+      });
+    }
+  };
+
+  const filteredTransactions = searchTerm
+    ? transactions.filter((transaction) =>
+        transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : transactions;
+
+  return (
+    // Search Bar 
+    <div className="container">
+      <div className="search-container">
+        <form className="search-box">
+          <div className="search-input-box">
+            <input
+              type="text"
+              placeholder="Search your Recent Transactions"
+              className="search-input"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <i className="fa-solid fa-magnifying-glass search-icon"></i>
+          </div>
+        </form>
+      </div>
+
+      {/* // form */}
+      <div id="submit-form">
+        <form onSubmit={addTransaction} className="input-box">
+         <div id= "input-box">
+         <input
+            id="date"
+            type="date"
+            name="date"
+            placeholder="Date"
+            className="form-input"
+            value={newTransaction.date}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="Description"
+            className="form-input"
+            value={newTransaction.description}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="category"
+            placeholder="Category"
+            className="form-input"
+            value={newTransaction.category}
+            onChange={handleInputChange}
+          />
+          <input
+            type="number"
+            name="amount"
+            placeholder="Amount"
+            className="form-input"
+            value={newTransaction.amount}
+            onChange={handleInputChange}
+          />
+         </div>
+         {/* // Button */}
+          <div>
+            <button type="submit" className="btn btn-primary">Add Transaction</button>
+          </div>
+        </form>
+      </div>
+{/* 
+      // Display Table */}
+      <div className="table">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Date</th>
+              <th scope="col">Description</th>
+              <th scope="col">Category</th>
+              <th scope="col">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredTransactions.map((transaction, index) => (
+              <tr key={index}>
+                <td>{transaction.date}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.category}</td>
+                <td>Ksh {transaction.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Transaction;
